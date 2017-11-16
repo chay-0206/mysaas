4b12d7f2221f378b5367199f8ac474bb04b66e32
hfq-chenyan <chenyan@huizhaofang.com>
2017-11-16 10:41:58 +0800

    路由优化，添加404页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import system from '@/system/index';
 import system_role from '@/system/role';
+
+import page404 from '@/components/404';
 
 Vue.use(Router);
 
 export default new Router({
+	mode: 'history',
 	routes: [
+		{ path: '*', component: page404 },
 		{
 			path: '',
 			name: 'system_role',
 			component: system_role
 		},
 		{
-			path: '/system/role',
-			name: 'system_role',
-			component: system_role
+			path: '/system',
+			component: system,
+			children: [{ path: 'role', component: system_role }]
 		}
 	]
 });
